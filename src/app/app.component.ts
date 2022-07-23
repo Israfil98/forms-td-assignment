6c496e9e43e0576a63e822c4151a280da16f13ab
Israfil98 <israfil.guseynov.98@inbox.ru>
2022-07-23 18:34:52 +0400

    complete assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  @ViewChild("form") formData: NgForm;
 
+  isSubmitted = false;
+  defaultAccount = "advanced";
+  userData = {
+    email: "",
+    account: "",
+    password: "",
+  };
+
+  onSubmit() {
+    console.log(this.formData);
+    this.isSubmitted = true;
+    this.userData = {
+      email: this.formData.value.email,
+      account: this.formData.value.account,
+      password: this.formData.value.password,
+    };
+  }
 }
